defee67154523311b33f52ca29ef604c85958693
yvvzh <135761301+yvvzh@users.noreply.github.com>
2024-02-09 09:50:56 +0100

    added dotenv config

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyALDLBUBMSTduJtnplwtOWYJ3lyE3qyrq4",
-    authDomain: "little-chat-js-firebase.firebaseapp.com",
-    databaseURL: "https://little-chat-js-firebase-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "little-chat-js-firebase",
-    storageBucket: "little-chat-js-firebase.appspot.com",
-    messagingSenderId: "473987508201",
-    appId: "1:473987508201:web:fc2d7b7907bc0940cc4d78",
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
     entry: "./src/index.js",
     mode: "development",
+    plugins: [new Dotenv()],
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
